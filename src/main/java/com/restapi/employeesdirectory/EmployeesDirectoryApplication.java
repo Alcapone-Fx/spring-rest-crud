7ec823843f6324a0378018c0c965c0aa9df034c9
Felix Navas <navasfx19@gmail.com>
2023-12-14 15:04:32 -0600

    :tada: Initial commit

---
+++ src/main/java/com/restapi/employeesdirectory/EmployeesDirectoryApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.employeesdirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesDirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesDirectoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restapi/employeesdirectory/EmployeesDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.employeesdirectory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesDirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
